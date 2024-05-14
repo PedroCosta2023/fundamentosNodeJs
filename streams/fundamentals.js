1c244c42e97147508e0daf123482211b69253ae4
PedroCosta2023 <132832807+PedroCosta2023@users.noreply.github.com>
2024-05-14 18:53:00 -0300

    Streams Fundamentals Classs

---
+++ streams/fundamentals.js
@@ -0,0 +1,13 @@
+// Netflix & Spotify
+
+// Importanção de clientes via CSV
+// 1gb - 1.000.000
+// POST /upload import.csv
+
+// 10mb/s - 100s
+
+// 100s -> Inserções no Banco de Dados
+
+// 10mb/s -> 10.000
+
+// Readble Streams / Whitable Streams
