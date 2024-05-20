7b999b820c923af2ac32e660645bc01a93ac1c3c
PedroCosta2023 <132832807+PedroCosta2023@users.noreply.github.com>
2024-05-20 20:07:56 -0300

    feat: foi alterado para que a criação seja feita através de requisições

--- src/server.js
+++ src/server.js
@@ -11,9 +11,21 @@ import { url } from 'node:inspector'
 const users = []
 
 
-const server = http.createServer((req,res) =>{
+const server = http.createServer( async (req,res) =>{
 
     const {method, url} = req
+
+    const buffers = []
+
+    for await (const chunk of req) {
+      buffers.push(chunk)
+    }
+  
+    try{
+        req.body = JSON.parse(Buffer.concat(buffers).toString())
+    } catch{
+        req.body = null
+    }
 
     if(method === 'GET' && url === "/users"){
         return res
@@ -22,11 +34,11 @@ const server = http.createServer((req,res) =>{
     }
 
     if(method === 'POST' && url === "/users"){
-
+        const { nome, email} = req.body
         users.push({
             id: 0,
-            nome: 'Pedro',
-            email: 'pedro@mail.com'
+            nome,
+            email
         })
 
         return res.writeHead(201).end()
